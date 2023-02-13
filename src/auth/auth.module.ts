5248cbec2eab99a2b15a5738791ec1f0d87e28f2
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-13 14:48:14 -0300

    auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController]
+})
+export class AuthModule {}
