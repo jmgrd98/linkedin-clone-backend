7c275bb565fb6f6d66c2792c3aa4b110890524ed
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-14 11:07:01 -0300

    Update feed.controller.ts

--- src/feed/controllers/feed/feed.controller.ts
+++ src/feed/controllers/feed/feed.controller.ts
@@ -3,50 +3,55 @@ import { UpdateResult, DeleteResult } from 'typeorm';
 import { Observable } from 'rxjs';
 import { FeedPost } from './../../models/post.interface';
 import { FeedService } from './../../services/feed.service';
-import { Post, Get, Patch, Put, Delete, Controller, Body, Query, Request } from '@nestjs/common';
+import {
+  Post,
+  Get,
+  Patch,
+  Put,
+  Delete,
+  Controller,
+  Body,
+  Query,
+  Request,
+} from '@nestjs/common';
 import { Param, UseGuards } from '@nestjs/common/decorators';
 @Controller('feed')
 export class FeedController {
-    constructor(
-        private feedService:FeedService
-    ) {}
+  constructor(private feedService: FeedService) {}
 
-    @UseGuards(JwtGuard)
-    
+  @UseGuards(JwtGuard)
+  @Post()
+  create(@Body() feedPost: FeedPost, @Request() req): Observable<FeedPost> {
+    return this.feedService.createPost(req.user, feedPost);
+  }
 
-    @Post()
-    create(@Body() feedPost:FeedPost, @Request() req): Observable<FeedPost> {
-        return this.feedService.createPost(req.user, feedPost);
-    }
+  @Get()
+  getAll(): Observable<FeedPost[]> {
+    return this.feedService.findAllPosts();
+  }
 
-    // @Get()
-    // getAll(): Observable<FeedPost[]> {
-    //     return this.feedService.findAllPosts();
-    // }
+  @Get()
+  findSelected(
+    @Query('take') take = 1,
+    @Query('skip') skip = 1,
+  ): Observable<FeedPost[]> {
+    take = take > 20 ? 20 : take;
+    return this.feedService.findPosts(take, skip);
+  }
 
-    @Get()
-    findSelected(
-        @Query('take') take:number = 1,
-        @Query('skip') skip:number = 1,
-        ): Observable<FeedPost[]> {
-        take = take > 20 ? 20 : take;       
-        return this.feedService.findPosts(take, skip);
-    }
+  @Get()
+  get() {}
 
-    @Get()
-    get(){
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() feedPost: FeedPost,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedPost);
+  }
 
-    }
-
-    @Put(':id')
-    update(
-        @Param('id') id:number,
-        @Body() feedPost: FeedPost): Observable<UpdateResult>{
-        return this.feedService.updatePost(id, feedPost);
-    }
-
-    @Delete(':id')
-    delete(@Param('id') id:number,): Observable<DeleteResult>{
-        return this.feedService.deletePost(id);
-    }
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.feedService.deletePost(id);
+  }
 }
