f61950a92c592b88ed660f4e33ee67eb6e41530e
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-24 12:42:49 -0300

    isCreatorGuard

---
+++ src/feed/guards/is-creator.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../auth/services/auth.service';
+import { FeedService } from '../services/feed.service';
+import { User } from '../../auth/models/user.interface';
+
+@Injectable()
+export class IsCreatorGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private feedService: FeedService,
+  ) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { user, params }: { user: User; params: { id: number } };
+  }
+}
