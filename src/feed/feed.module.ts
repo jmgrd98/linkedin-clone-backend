c54ddaa65c02e036aea32139a6f071af281ef059
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-24 11:49:43 -0300

    isCreatorGuard

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { FeedService } from './services/feed.service';
 import { FeedController } from './controllers/feed/feed.controller';
 import { FeedPostEntity } from './models/post.entity';
+import { AuthModule } from '../auth/auth.module';
+import { IsCreatorGuard } from "./guards/is-creator.guard";
 @Module({
-  providers: [FeedService],
+  providers: [FeedService, IsCreatorGuard],
   controllers: [FeedController],
-  imports: [TypeOrmModule.forFeature([FeedPostEntity])],
+  imports: [AuthModule, TypeOrmModule.forFeature([FeedPostEntity])],
 })
 export class FeedModule {}
